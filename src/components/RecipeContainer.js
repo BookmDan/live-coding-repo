525978c82fa306b304cc90d2c4c7b7389c094265
BookmDan <danoh329@gmail.com>
2023-10-24 18:02:26 -0700

    have the timeFilter down

--- src/components/RecipeContainer.js
+++ src/components/RecipeContainer.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import Recipe from "./Recipe";
 import RecipeCard from "./RecipeCard";
+import Challenge1 from "./Challenge1";
 
-function RecipeContainer({ apiRecipes, search, onAddRecipeClick, effortAmount }){
+function RecipeContainer({ apiRecipes, search, onAddRecipeClick, effortAmount }) {
+    const [timeFilter, setTimeFilter] = useState("")
+    const base = apiRecipes.baseUri
+    
+    const handleFilter = () => {
+        const timeFiltered = apiRecipes.results.filter(result => result.readyInMinutes <= parseInt(timeFilter))
+        console.log(timeFiltered)
+        
+        return null
+        // console.log(timeFilter)
+    }
 
-    const base=apiRecipes.baseUri
-
-    const recipesToDisplay=apiRecipes.results.filter(result=>result.title.toLowerCase().includes(search.toLowerCase()))
-    .map(recipe=>{
+    const recipesToDisplay = apiRecipes.results.filter(result => result.title.toLowerCase().includes(search.toLowerCase()))
+    const returnRecipes = recipesToDisplay.map(recipe=>{
         return <Recipe 
                     key={recipe.id} 
                     id={recipe.id} 
@@ -20,10 +29,10 @@ function RecipeContainer({ apiRecipes, search, onAddRecipeClick, effortAmount })
                 />
     })
 
-
     return(
         <div id="recipe-container" className="container">
-            {recipesToDisplay}
+            <Challenge1 timeFilter={timeFilter} setTimeFilter={setTimeFilter} handleFilter={handleFilter} />
+            {}
         </div>
     )
 }
